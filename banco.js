d71e424e2063141c55d506f55b1b9dd92d6faf78
Professor Jefferson de Castro <61990009+ProfJrcastro@users.noreply.github.com>
2026-03-24 21:46:09 -0300

    Update banco.js

---
+++ banco.js
@@ -0,0 +1,17 @@
+const BANCO_DADOS = {
+  "config": { 
+      "email": "seu-email@firjan.com.br" 
+  },
+  "questoes": [
+    {
+      "pergunta": "Exemplo de Questão 01: Qual a cor principal do SENAI?",
+      "opcoes": ["Verde", "Amarelo", "Azul", "Rosa", "Preto"],
+      "correta": "C"
+    },
+    {
+      "pergunta": "Exemplo de Questão 02: O que significa a sigla NR?",
+      "opcoes": ["Norma Regional", "Norma Regulamentadora", "Nova Regra", "Nível de Risco", "Nenhuma"],
+      "correta": "B"
+    }
+  ]
+};
